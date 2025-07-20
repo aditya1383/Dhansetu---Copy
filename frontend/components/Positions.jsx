f6bd5e10d6b8a978c1320d422acd467680b90a62
aditya1383 <aditchaudhary61@gmail.com>
2025-07-20 23:31:10 +0530

    Update Positions.jsx

--- frontend/components/Positions.jsx
+++ frontend/components/Positions.jsx
@@ -7,7 +7,7 @@ const Positions = () => {
   const [allPositions, setAllPositions] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:3002/allPositions").then((res) => {
+    axios.get("https://dhansetu-backend.onrender.com/allPositions").then((res) => {
       setAllPositions(res.data);
     })
   }, [])
