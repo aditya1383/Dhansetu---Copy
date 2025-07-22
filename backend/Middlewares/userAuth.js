a5bc511730b7d0626f4ce96713d8e8a16d3d57d4
aditya1383 <aditchaudhary61@gmail.com>
2025-07-22 11:08:49 +0530

    Update userAuth.js
    no change

--- backend/Middlewares/userAuth.js
+++ backend/Middlewares/userAuth.js
@@ -16,3 +16,21 @@ module.exports.userAuth =  async (req, res, next) => {
     next();
     })
 }
+
+
+// after deploy
+// module.exports.userAuth = async (req, res, next) => {
+//   try {
+//     const token = req.cookies?.token;
+//     if (!token) return res.status(401).json({ status: false });
+
+//     const data = jwt.verify(token, process.env.TOKEN_KEY);
+//     const user = await User.findById(data.id);
+//     if (!user) return res.status(401).json({ status: false });
+
+//     req.user = user;
+//     next();
+//   } catch (err) {
+//     return res.status(401).json({ status: false });
+//   }
+// };
